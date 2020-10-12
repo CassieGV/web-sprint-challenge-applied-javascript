4289b242e005ad01fa9c336c953570f3ccfe2ad4
Cassandra Villarreal <cass2u94@gmail.com>
2020-10-11 21:00:42 -0400

    Header... easy

--- components/Header.js
+++ components/Header.js
@@ -11,4 +11,33 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+function Header(dateData, tempData) {
+    let headerContainer = document.querySelector(".header-container")
+
+    let header = document.createElement("div");
+    header.classList.add("header");
+    headerContainer.appendChild(header)
+
+    
+    let date = document.createElement("span");
+    date.classList.add("date");
+    date.textContent = dateData
+    header.appendChild(date);
+    
+    let title = document.createElement("h1");
+    console.log(document.querySelector("title"))
+    let Title = document.querySelector("title").textContent
+    title.textContent = Title;
+    header.appendChild(title);
+
+    let temp = document.createElement("span");
+    temp.classList.add("temp");
+    temp.textContent = tempData
+    header.appendChild(temp);
+
+    console.log(header)
+    return header
+}
+
+Header("MARCH 28, 2020", "98°")
+
